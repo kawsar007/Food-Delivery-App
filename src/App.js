1216678800f6147b653b45a4c98d90547af5c8ca
kawsar007 <imkawsar007@gmail.com>
2022-01-09 22:05:01 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header Section */}
+      <Header />
+      {/* Main Components */}
+
+      {/* Bottom Menu */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,38 @@
+import { BarChart, SearchRounded, ShoppingCartRounded } from '@mui/icons-material';
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <img src="https://thumbs.dreamstime.com/b/food-logo-smile-label-company-grocery-store-friendly-vector-illustration-smiling-mouth-symbol-135565322.jpg" alt="logo" className="logo"/>
+
+            <div className="inputBox">
+                <SearchRounded className="searchIcon" />
+                <input type="text" placeholder="Search"/>
+            </div>
+
+            {/* Shopping cart icon */}
+            <div className="shoppingCart">
+                <ShoppingCartRounded className="cart"/>
+                <div className="cart_content">
+                    <p>3</p>
+                </div>
+            </div>
+
+            {/* Profile icon */}
+            <div className="profileContainer">
+                <div className="imgBox">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNOhpV67XSI4Vz5Z_L7XoWiH7UzZQDBTzS3g&usqp=CAU" alt="" className="profilePic"/>
+                </div>
+
+                <h2 className="userName">Kawsar Mia</h2>
+            </div>
+
+            <div className="toggleMenu">
+                <BarChart className="toggleIcon" />
+            </div>
+        </header>
+    )
+}
+
+export default Header
